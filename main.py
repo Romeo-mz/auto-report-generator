937696455f39502d93ee328f07a38a7efd6eddd5
romeo-mz <romeo.martinez@orange.fr>
2023-05-19 21:41:33 +0200

    Transform pdf text using a method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from pdf_processing.pdf_extractor import extract_text_from_pdf
+from pdf_processing.pdf_extractor import *
 
 def main():
     pdf_file_path = 'input/enonce_TP.pdf'  # Chemin vers le fichier PDF à traiter
     extracted_text = extract_text_from_pdf(pdf_file_path)
-    print(extracted_text)
+    # print(extracted_text)
+    cr = transform_output_to_report(extracted_text)
 
+    print(cr)
 if __name__ == '__main__':
     main()
--- pdf_processing/pdf_extractor.py
+++ pdf_processing/pdf_extractor.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import re 
 
 def extract_text_from_pdf(file_path):
     with open(file_path, 'rb') as file:
@@ -7,3 +8,28 @@ def extract_text_from_pdf(file_path):
         for page in reader.pages:
             text += page.extract_text()
         return text
+
+def transform_output_to_report(output):
+    report = ""
+    lines = output.split("\n")
+    section = ""
+    section_dict = {}
+
+
+    for line in lines:
+        line = line.strip()
+
+        # Vérifier si la ligne est un titre ou une question
+        if re.match(r"^\d+\. .*", line):
+            section = line.split(". ", 1)[1]
+            line = ""
+        elif line.startswith("a.") or re.match(r"^\d+\..*", line):
+            section = re.sub(r"^\w+\.\s*", "", line)
+            line = ""
+
+        # Ajouter la ligne au compte rendu uniquement si une section est déjà présente et la ligne n'est pas vide
+        if section and line and section not in section_dict:
+            report += f"{section} : {line}\n\n"
+            section_dict[section] = True
+
+    return report.strip()
